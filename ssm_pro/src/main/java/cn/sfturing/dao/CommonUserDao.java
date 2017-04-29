42ab4c6109a33e360abaeb10293eb6d478345f54
sfturing <sfturing@gmail.com>
2017-04-29 22:18:12 +0800

    login

--- ssm_pro/src/main/java/cn/sfturing/dao/CommonUserDao.java
+++ ssm_pro/src/main/java/cn/sfturing/dao/CommonUserDao.java
@@ -10,22 +10,24 @@ public interface CommonUserDao {
 	/*
 	 * 查找用户
 	 */
-	public CommonUser findCommonUserByUserId(long id) throws Exception;
+	public CommonUser findCommonUserByUserId(long userId) throws Exception;
 	
 	public CommonUser findCommonUserByUserIdenf(String userIdenf) throws Exception;
 
 	public CommonUser findCommonUserByUserName(String userName) throws Exception;
 
-	public CommonUser findCommonUserByEmail(String email) throws Exception;
+	public CommonUser findCommonUserByEmail(String userEmail) throws Exception;
 
-	public CommonUser findCommonUserByPhone(String phone) throws Exception;
+	public CommonUser findCommonUserByMobile(String userMobile) throws Exception;
 
 	/*
 	 * 插入更新和删除
 	 */
 	public int insertCommonUser(CommonUser CommonUser) throws Exception;
-
+	//通过id删除
 	public void deleteCommonUser(long id) throws Exception;
+	//通过身份证号删除
+	public void deleteCommonUserByIdenf(String userIdenf) throws Exception;
 
 	/*
 	 * 更改用户密码、手机号码以及激活邮箱
